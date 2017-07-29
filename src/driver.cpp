a1e1d3290eb63e0b8b17034ca7b01a76a1ab5e50
Alec Petridis <chop@quenda.net>
2017-07-29 09:32:50 -0700

    Will now calibrate offsets when program starts

--- src/driver.cpp
+++ src/driver.cpp
@@ -27,17 +27,19 @@ std::list<int> motors = {MOTOR_1, MOTOR_2, MOTOR_3, MOTOR_4};
 #define SSTR( x ) static_cast< std::ostringstream & >( \
 		( std::ostringstream() << std::dec << x ) ).str()
 
+float pitchOffset,rollOffset;
+
 
 float getRoll(RTIMU *imu) {
 	RTIMU_DATA imuData = imu->getIMUData();
 
-	return imuData.fusionPose.x();
+	return imuData.fusionPose.x() - rollOffset;
 }
 
 float getPitch(RTIMU *imu) {
 	RTIMU_DATA imuData = imu->getIMUData();
 
-	return imuData.fusionPose.z();
+	return imuData.fusionPose.z() - pitchOffset;
 }
 
 
@@ -52,19 +54,37 @@ float getForwardVelocity  (RTIMU *imu) {
 	RTIMU_DATA imuData = imu->getIMUData();
 
 	return imuData.accel.z();
-	
-return 0;
+
+	return 0;
 }
 
 
-void setup() {
+
+
+
+
+
+
+
+void setOffsets(RTIMU* imu) {
+	std::cout << "Doing offset calibration, place on flat surface" << std::endl;
+
+	pitchOffset = getPitch(imu);
+	rollOffset = getRoll(imu);
+
+
+
+}
+
+
+RTIMU* setup() {
 
 	RTIMUSettings *settings = new RTIMUSettings("RTIMULib");
 
-	RTIMU *imu = RTIMU::createIMU(settings);
+	imu = RTIMU::createIMU(settings);
 
 
-
+	setOffsets(imu);
 	if ((imu == NULL) || (imu->IMUType() == RTIMU_TYPE_NULL)) {
 		printf("No IMU found\n");
 		exit(1);
@@ -77,49 +97,11 @@ void setup() {
 	imu->setAccelEnable(true);
 
 	gpioInitialise();
-	for(const auto& motor : motors) {
-		gpioSetMode(motor, PI_OUTPUT);
-		drone::sendPWM(motor, 1100);
-	}
+
+	return imu;
 }
 
-void loopManual() {
 
-	std::string line;
-
-	std::getline(std::cin, line); // Blocks until something interesting comes into stdin.
-
-
-	//switch(getch()) { // Using newer, less obsolete method which doesn't require system calls now
-	if(line[0] > -1) {
-
-		switch(line[0]) {
-			case '1':
-				std::getline(std::cin, line);
-				drone::sendPWM ( MOTOR_1, std::stoi(line) );
-				break;
-			case '2':
-				std::getline(std::cin, line);
-				drone::sendPWM ( MOTOR_2, std::stoi(line) );
-				break;
-			case '3':
-				std::getline(std::cin, line );
-				drone::sendPWM ( MOTOR_3, std::stoi(line) );
-				break;
-			case '4':
-				std::getline(std::cin, line);
-				drone::sendPWM ( MOTOR_4, std::stoi(line) );
-				break;
-			default:
-				if(line == "leftright") {
-					std::getline(std::cin, line);
-					drone::appendPWM(MOTOR_2, std::stoi(line));
-					drone::appendPWM(MOTOR_3, std::stoi(line));
-				}
-				break;
-		}
-	}
-}
 
 
 
@@ -181,18 +163,9 @@ void loopStable(RTIMU *imu) {
 
 
 int main ( ) {
-	setup();
-	testIMU();
-	while (true) {
-		//	loopManual();
-	}
+	RTIMU* imu = setup();
 
-	/*
-	   gpioInitialise();
-	   gpioSetMode(18, PI_OUTPUT);
-	   drone::sendPWM ( 18, 1500);
-	   while (true); // Keep running until terminated
-	   */
+	while (true) {}
 }
 
 
