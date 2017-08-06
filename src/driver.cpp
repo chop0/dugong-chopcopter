bdb1c670ce3dada1ffea5ecac307fd0c44461eb5
Alec Petridis <chop@quenda.net>
2017-08-06 11:34:06 -0700

    Another algo update

--- src/driver.cpp
+++ src/driver.cpp
@@ -149,14 +149,14 @@ int pwm4 = 1100;
 
 void loopStable(RTIMU *imu) {
 
-	if(std::abs(getRoll(imu)) >= std::abs(64/getRightwardVelocity(imu))) {
+	if(std::abs(getRoll(imu)) >= std::abs(getRightwardVelocity(imu))) {
 		while(std::abs(getRoll(imu)) >= rollCorrectionAngle(imu)) {
 			pwm1 = pwm1 - rollCorrectionWidth(imu);
 			pwm4 = pwm4 - rollCorrectionWidth(imu);
 			sleep_for(seconds(1));
 		}
 	}
-	if(std::abs(getPitch(imu)) >= std::abs(64/getForwardVelocity(imu))) {
+	if(std::abs(getPitch(imu)) >= std::abs(getForwardVelocity(imu))) {
 		while(std::abs(getPitch(imu)) >= pitchCorrectionAngle(imu)) {
 			pwm1 = pwm1 - pitchCorrectionWidth(imu);
 			pwm3 = pwm3 - pitchCorrectionWidth(imu);
@@ -179,7 +179,7 @@ void loopStable(RTIMU *imu) {
 		std::cout << "Motor 3: " << pwm3 << std::endl;
 		std::cout << "Motor 4: " << pwm4 << std::endl;
 		std::cout << std::abs(getRoll(imu)) << std::endl;
-		std::cout <<  std::abs(64/getRightwardVelocity(imu)) << std::endl;
+		std::cout <<  std::abs(getRightwardVelocity(imu)) << std::endl;
 
 
 
